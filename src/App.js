e9d2dfbec96382c17fa3823953b903acea0e8b07
daysaregray <graydays505@gmail.com>
2023-03-09 16:04:20 +0900

    ver01

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Router1 from './components/Router1';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+    return (
+        <div className="App">
+            <Header/>
+            <Router1/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default App;
